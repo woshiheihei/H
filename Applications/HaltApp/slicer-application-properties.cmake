38f84661d4527feb59939365b49248e51f6ae5f2
weibao <wei.bao@saint-medical.com>
2025-10-24 14:50:39 +0800

    feat: Update splash screen image reference and add HALT logo

--- Applications/HaltApp/slicer-application-properties.cmake
+++ Applications/HaltApp/slicer-application-properties.cmake
@@ -24,7 +24,7 @@ set(DESCRIPTION_FILE
   )
 
 set(LAUNCHER_SPLASHSCREEN_FILE
-  "${CMAKE_CURRENT_LIST_DIR}/Resources/Images/SplashScreen.png"
+  "${CMAKE_CURRENT_LIST_DIR}/Resources/Images/HALT_SplashScreen.png"
   )
 set(APPLE_ICON_FILE
   "${CMAKE_CURRENT_LIST_DIR}/Resources/Icons/DesktopIcon.icns"
